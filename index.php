2efc73b702b7b34cbcd55435fd6fc0408e2b2eb4
Suhail <suhailarfaath@gmail.com>
2022-10-13 22:17:22 -0500

    2nd commit by CSM

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $user_data = check_login($con);
     <div class="container-fluid">
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">C S M</a>
+            <a class="navbar-brand" href="#">C S M Test</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
